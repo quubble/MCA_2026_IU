fc6b1cae66dee9b293b89047bc96d260521a4839
quubble <45556712+quubble@users.noreply.github.com>
2026-07-14 20:17:36 +0530

    Add program to check for valid triangle angles

---
+++ Bridge_Course/C_Practice_Programs/find_Valid_Triangle.c
@@ -0,0 +1,17 @@
+// ## (G) Valid Triangle
+
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+
+    printf("Enter three angles: ");
+    scanf("%d%d%d", &a, &b, &c);
+
+    if (a + b + c == 180)
+        printf("Valid Triangle");
+    else
+        printf("Invalid Triangle");
+
+    return 0;
+}
