fc0ec7cd3be210bbd404471c2bbc7cc7bd2d87fb
quubble <45556712+quubble@users.noreply.github.com>
2026-07-14 20:16:02 +0530

    Add program to find the youngest of three people

---
+++ Bridge_Course/C_Practice_Programs/find_youngest.c
@@ -0,0 +1,19 @@
+## (F) Youngest of Three
+
+#include <stdio.h>
+
+int main() {
+    int ram, shyam, ajay;
+
+    printf("Enter ages of Ram, Shyam and Ajay: ");
+    scanf("%d%d%d", &ram, &shyam, &ajay);
+
+    if (ram < shyam && ram < ajay)
+        printf("Ram is Youngest");
+    else if (shyam < ram && shyam < ajay)
+        printf("Shyam is Youngest");
+    else
+        printf("Ajay is Youngest");
+
+    return 0;
+}
