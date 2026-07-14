2aeac34679b8e109ed6970f7e8a0614f424c0047
quubble <45556712+quubble@users.noreply.github.com>
2026-07-14 20:05:29 +0530

    Add profit and loss calculation program

---
+++ Bridge_Course/C_Practice_Programs/calculate_profit_loss.c
@@ -0,0 +1,22 @@
+//Calculate_profit_loss
+
+#include <stdio.h>
+
+int main() {
+    float cp, sp;
+
+    printf("Enter Cost Price: ");
+    scanf("%f", &cp);
+
+    printf("Enter Selling Price: ");
+    scanf("%f", &sp);
+
+    if (sp > cp)
+        printf("Profit = %.2f", sp - cp);
+    else if (cp > sp)
+        printf("Loss = %.2f", cp - sp);
+    else
+        printf("No Profit No Loss");
+
+    return 0;
+}
