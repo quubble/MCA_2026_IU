a5c89066d4f27901193009935ab9024824ea927b
quubble <45556712+quubble@users.noreply.github.com>
2026-07-14 22:47:44 +0530

    Add program to check for special symbols in characters
    This program checks if a given character is a special symbol based on its ASCII value.

---
+++ Bridge_Course/C_Practice_Programs/special_char_or_not.c
@@ -0,0 +1,25 @@
+//Check whether a character is a special symbol
+
+#include <stdio.h>
+
+int main() {
+    char ch;
+
+    printf("Enter a character: ");
+    scanf(" %c", &ch);
+
+    ((ch >= 0 && ch <= 47) ||
+     (ch >= 58 && ch <= 64) ||
+     (ch >= 91 && ch <= 96) ||
+     (ch >= 123 && ch <= 127)) ?
+
+        printf("Special symbol\n") :
+        printf("Not a special symbol\n");
+
+    return 0;
+}
+
+/* Sample Output:
+Enter a character: @
+Special symbol
+*/
