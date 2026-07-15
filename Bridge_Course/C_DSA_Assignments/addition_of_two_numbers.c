2331f0a6b33b92fb7c8a23731d02fab8153f3a7e
quubble <45556712+quubble@users.noreply.github.com>
2026-07-15 15:23:51 +0530

    Add program for addition of two numbers
    Implement a program to add two numbers and display the result.

---
+++ Bridge_Course/C_DSA_Assignments/addition_of_two_numbers.c
@@ -0,0 +1,21 @@
+ad//Addition of two numbers
+
+#include <stdio.h>
+
+int main(void) {
+    
+   // variable declaration 
+    int a = 4;
+    int c = 6;
+    int  sum = 0;
+    
+    //Adiition of numbers
+    sum = a+c;
+    
+
+// display 
+    printf("addition = %d\n", sum);
+    
+
+    return 0;
+}
