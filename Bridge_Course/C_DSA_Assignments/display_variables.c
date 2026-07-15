8cd1aa468cfb7b16d5e072b6368ea61c352a40a2
quubble <45556712+quubble@users.noreply.github.com>
2026-07-15 15:18:04 +0530

    Add display_variables.c to show variable types

---
+++ Bridge_Course/C_DSA_Assignments/display_variables.c
@@ -0,0 +1,19 @@
+//display_variables
+
+#include <stdio.h>
+int main(void) {
+    
+   // variable declaration 
+    int a = 4;
+    char c = 'a';
+    float f = 1.0;
+    double d = 23344.888888;
+
+// display variables
+    printf("a = %d\n", a);
+    printf("c = %c\n", c);
+    printf("f = %f\n", f);
+    printf("d = %lf\n", d);
+
+    return 0;
+}
