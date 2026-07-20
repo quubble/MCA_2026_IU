40d199a24bff8f7ab9c66e9e6e075e636b9d183b
quubble <45556712+quubble@users.noreply.github.com>
2026-07-20 23:06:06 +0530

    Added Pascal's Triangle implementation program in C assignment

---
+++ Bridge_Course/C_DSA_Assignments/pascal_triangle.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int rows, i, j;
+    long val;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    for (i = 0; i < rows; i++) {
+        // Print spaces for alignment
+        for (j = 0; j < rows - i - 1; j++) {
+            printf("  ");
+        }
+
+        val = 1;
+        // Print values in the row
+      // important logic
+        for (j = 0; j <= i; j++) {
+            printf("%4lld", val);
+          //Using the formula of binomial coefficient here
+            val = val * (i - j) / (j + 1);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
