df7536ca831c4b6b596fd0baa73dabbb68fbac7d
quubble <45556712+quubble@users.noreply.github.com>
2026-07-23 21:51:43 +0530

    Add program to find maximum number in array

---
+++ Bridge_Course/C_Practice_Programs/find_max_number_in_array.c
@@ -0,0 +1,21 @@
+//find_max_number_in_array
+
+#include<stdio.h>
+
+int main(){
+  int arr[] = {23, 12, 450, 3, 63, 14};
+
+  //finding the array size
+  int n = sizeof(arr) / sizeof(arr[0]);
+
+  int max = arr[0];
+  
+  for(int i=0; i<= n-1; i++){
+    
+    if(arr[i] > max){
+      max = arr[i];
+    }
+  }
+  printf("Maximum number is %d\n", max);
+  return 0;
+}
