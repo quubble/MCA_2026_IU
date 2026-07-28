4986a09599c9839b5e563e3220b77d45864d571c
quubble <45556712+quubble@users.noreply.github.com>
2026-07-28 22:25:22 +0530

    Add function to get concatenation of an array
    Implement function to concatenate an array with itself.

---
+++ Bridge_Course/Leetcode/concatenation-of-array.c
@@ -0,0 +1,26 @@
+//https://leetcode.com/problems/concatenation-of-array
+
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* getConcatenation(int* nums, int numsSize, int* returnSize) {
+    // Allocate memory for a new array of size 2 * numsSize
+    int* ans = (int*)malloc(sizeof(int) * 2 * numsSize);
+
+    // Loop through each element of the original array
+    for (int i = 0; i < numsSize; i++) {
+        // Copy nums[i] into the first half of ans
+        ans[i] = nums[i];
+
+        // Copy nums[i] into the second half of ans
+        ans[i + numsSize] = nums[i];
+    }
+
+    // Set the size of the returned array
+    *returnSize = 2 * numsSize;
+
+    // Return the newly created array
+    return ans;
+}
