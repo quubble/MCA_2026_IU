4b2dae0cb0aa76346ee63a1bb96ad1a4ea669793
quubble <45556712+quubble@users.noreply.github.com>
2026-08-14 14:52:50 +0530

    Implement while loop to print numbers 1 to 10
    This program prints all numbers from 1 to 10 using a while loop.

---
+++ Loops/while_print_num.c
@@ -0,0 +1,13 @@
+ //Print all numbers from 1 to 10 using a loop.
+
+#include<stdio.h>
+int main(){
+int i =1;
+
+printf("the vaue of i is:\n");
+while(i <= 10){
+    printf("%d\n", i);
+    i++;
+    }
+ return 0;
+}
