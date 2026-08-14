f1c7d45cb975fc29d2c6ce78acde77f6574d59d4
quubble <45556712+quubble@users.noreply.github.com>
2026-08-14 14:54:18 +0530

    Add while loop to print numbers in reverse
    This program prints numbers from 10 down to 1 in reverse order.

---
+++ Loops/while_print_num_reverse.c
@@ -0,0 +1,13 @@
+//Print numbers from 10 down to 1 in reverse order.
+
+#include<stdio.h>
+int main(){
+int i =10;
+
+printf("the vaue of i is:\n");
+while(i >= 1){
+    printf("%d\n", i);
+    i--;
+    }
+ return 0;
+}
